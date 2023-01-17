fc9b18fbbf865affdd74cc1f8dcd38017451a03c
MilanTibadiya <tibadiyamilan@gmail.com>
2023-01-17 14:47:05 +0530

    Task-11( The filter feature)

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,37 +1,47 @@
 import React, { useState } from "react";
-import './Expenses.css'
-import ExpenseItem from './ExpenseItem';
+
+import "./Expenses.css";
+import ExpenseItem from "./ExpenseItem";
 import Card from "./Card";
 import ExpensesFilter from "./ExpenseFilter";
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState('2023');
+  const [filteredYear, setFilteredYear] = useState("2023");
 
-  const filterChangeHandler = selectedYear => {
+  const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
-  }
+  };
 
-    return ( 
-        <Card className='expenses'>
-          <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-          {
-            props.item.map( expense => (
-              <ExpenseItem
-              key={expense.id}
-              title={expense.title}
-              amount={expense.amount}
-              date={expense.date}
-            />
-            ))
-          }
-           
+  const filteredExpenses = props.item.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  let expensesContent = <p>No expenses found</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      {expensesContent}
+
       {/* <ExpenseItem
         title={props.item[1].title}
         amount={props.item[1].amount}
         date={props.item[1].date}/> */}
-
-        </Card>
-    )
-}
+    </Card>
+  );
+};
 
 export default Expenses;
