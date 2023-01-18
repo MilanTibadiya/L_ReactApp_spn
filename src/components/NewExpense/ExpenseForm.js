527a3c00cc962571d83348081e033101f7ab4399
MilanTibadiya <tibadiyamilan@gmail.com>
2023-01-18 12:20:55 +0530

    Task-14(Chart App, fixing small bug)

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -27,7 +27,7 @@ const ExpenseForm = (props) => {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate)
     }
 
